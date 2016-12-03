19457625e792b3a17a9365325b8b98d663468f2f
Soutaro Matsumoto <matsumoto@soutaro.com>
2016-12-03 09:06:07 +0900

    :muscle:

--- lib/jack_and_the_elastic_beanstalk/eb.rb
+++ lib/jack_and_the_elastic_beanstalk/eb.rb
@@ -3,7 +3,6 @@ module JackAndTheElasticBeanstalk
     attr_reader :application_name
     attr_reader :logger
     attr_reader :client
-    attr_reader :env_stack
     attr_accessor :timeout
 
     def initialize(application_name:, logger:, client:)
@@ -15,134 +14,176 @@ module JackAndTheElasticBeanstalk
     end
 
     def environments
-      client.describe_environments(application_name: application_name, include_deleted: false).environments
-    end
-
-    def set_current_environment(environment)
-      env_stack.push environment
-      yield
-    ensure
-      env_stack.pop
-    end
-
-    def current_environment
-      env_stack.last || raise("set_env(environment) to set current env")
-    end
-
-    def env_vars
-      config = client.describe_configuration_settings(application_name: application_name, environment_name: current_environment).configuration_settings.first
-      config.option_settings.each.with_object({}) do |option, hash|
-        if option.namespace == "aws:elasticbeanstalk:application:environment"
-          hash[option.option_name] = option.value
+      @environments = client.describe_environments(application_name: application_name, include_deleted: false).environments.map {|env|
+        Environment.new(application_name: application_name,
+                        environment_name: env.environment_name,
+                        logger: logger,
+                        client: client).tap do |e|
+          e.timeout = timeout
+          e.data = env
         end
-      end
+      }
     end
 
-    def set_env_vars(env)
-      current_env_vars = env_vars
+    def refresh
+      @environments = nil
+    end
 
-      if env.all? {|key, value| value && current_env_vars[key] == value }
-        logger.info("jeb::eb") { "Env vars looks like identical; skip" }
-      else
-        logger.info("jeb::eb") { "Updating environment variables" }
+    class Environment
+      attr_reader :application_name
+      attr_reader :logger
+      attr_reader :client
+      attr_reader :environment_name
+      attr_accessor :timeout
 
-        options_to_update = []
-        options_to_remove = []
+      def initialize(application_name:, logger:, client:, environment_name:)
+        @application_name = application_name
+        @logger = logger
+        @client = client
+        @environment_name = environment_name
+        @timeout = 600
+      end
 
-        env.each do |key, value|
-          if value
-            options_to_update << {
-              namespace: "aws:elasticbeanstalk:application:environment",
-              option_name: key.to_s,
-              value: value.to_s
-            }
-          else
-            options_to_remove << {
-              namespace: "aws:elasticbeanstalk:application:environment",
-              option_name: key.to_s
-            }
+      def refresh
+        @data = nil
+        @configuration_setting = nil
+      end
+
+      def data=(v)
+        @data = v
+      end
+
+      def data
+        @data ||= client.describe_environments(application_name: application_name).environments.find {|env|
+          env.environment_name == environment_name
+        }
+      end
+
+      def configuration_setting
+        @configuration_setting ||= client.describe_configuration_settings(application_name: application_name, environment_name: environment_name).configuration_settings.first
+      end
+
+      def env_vars
+        configuration_setting.option_settings.each.with_object({}) do |option, hash|
+          if option.namespace == "aws:elasticbeanstalk:application:environment"
+            hash[option.option_name] = option.value
           end
         end
-
-        client.update_environment(application_name: application_name,
-                                  environment_name: current_environment,
-                                  option_settings: options_to_update,
-                                  options_to_remove: options_to_remove)
       end
-    end
 
-    def synchronize_update(timeout: self.timeout)
-      logger.info("jeb::eb") { "Synchronizing update started... (timeout = #{timeout})" }
+      def set_env_vars(env)
+        need_update = env.all? {|key, value|
+          if value
+            env_vars[key] == value
+          else
+            !env_vars.key?(key)
+          end
+        }
 
-      yield if block_given?
-
-      start = Time.now
-
-      while true
-        st = status
-
-        logger.info("jeb::eb") { "#{current_environment}:: status=#{st}" }
-
-        case st
-        when "Ready"
-          break
-        when "Updating"
-          # ok
+        if need_update
+          logger.info("jeb::eb") { "Env vars looks like identical; skip" }
         else
-          raise "Unexpected status: #{st}"
-        end
+          logger.info("jeb::eb") { "Updating environment variables" }
 
-        if Time.now - start > timeout
-          raise "Timeout exceeded"
-        end
+          options_to_update = []
+          options_to_remove = []
 
-        sleep 10
+          env.each do |key, value|
+            if value
+              options_to_update << {
+                namespace: "aws:elasticbeanstalk:application:environment",
+                option_name: key.to_s,
+                value: value.to_s
+              }
+            else
+              options_to_remove << {
+                namespace: "aws:elasticbeanstalk:application:environment",
+                option_name: key.to_s
+              }
+            end
+          end
+
+          client.update_environment(application_name: application_name,
+                                    environment_name: environment_name,
+                                    option_settings: options_to_update,
+                                    options_to_remove: options_to_remove)
+
+          refresh
+        end
       end
 
-      logger.info("jeb::eb") { "Synchronized in #{(Time.now - start).to_i} seconds" }
-    end
+      def synchronize_update(timeout: self.timeout)
+        logger.info("jeb::eb") { "Synchronizing update started... (timeout = #{timeout})" }
 
-    def configuration_setting
-      client.describe_configuration_settings(application_name: application_name, environment_name: current_environment).configuration_settings.first
-    end
+        yield if block_given?
 
-    def scale
-      option_settings = configuration_setting.option_settings
+        start = Time.now
 
-      min = option_settings.find {|option| option.namespace == "aws:autoscaling:asg" && option.option_name == "MinSize" }.value.to_i
-      max = option_settings.find {|option| option.namespace == "aws:autoscaling:asg" && option.option_name == "MaxSize" }.value.to_i
+        while true
+          refresh
+          st = status
 
-      min...max
-    end
+          logger.info("jeb::eb") { "#{environment_name}:: status=#{st}" }
 
-    def status
-      environments.find {|env| env.environment_name == current_environment }.status
-    end
+          case st
+          when "Ready"
+            break
+          when "Updating"
+            # ok
+          else
+            raise "Unexpected status: #{st}"
+          end
 
-    def set_scale(scale)
-      if scale.is_a?(Integer)
-        scale = scale...scale
+          if Time.now - start > timeout
+            raise "Timeout exceeded"
+          end
+
+          sleep 10
+        end
+
+        logger.info("jeb::eb") { "Synchronized in #{(Time.now - start).to_i} seconds" }
       end
 
-      if self.scale == scale
-        logger.info("jeb::eb") { "New scale is identical to current scale; skip" }
-      else
-        logger.info("jeb::eb") { "Scaling to #{scale}" }
+      def scale
+        option_settings = configuration_setting.option_settings
 
-        client.update_environment(application_name: application_name,
-                                  environment_name: current_environment,
-                                  option_settings: [
-                                    {
-                                      namespace: "aws:autoscaling:asg",
-                                      option_name: "MinSize",
-                                      value: scale.begin.to_s
-                                    },
-                                    {
-                                      namespace: "aws:autoscaling:asg",
-                                      option_name: "MaxSize",
-                                      value: scale.end.to_s
-                                    }
-                                  ])
+        min = option_settings.find {|option| option.namespace == "aws:autoscaling:asg" && option.option_name == "MinSize" }.value.to_i
+        max = option_settings.find {|option| option.namespace == "aws:autoscaling:asg" && option.option_name == "MaxSize" }.value.to_i
+
+        min...max
+      end
+
+      def status
+        data.status
+      end
+
+      def set_scale(scale)
+        if scale.is_a?(Integer)
+          scale = scale...scale
+        end
+
+        if self.scale == scale
+          logger.info("jeb::eb") { "New scale is identical to current scale; skip" }
+        else
+          logger.info("jeb::eb") { "Scaling to #{scale}" }
+
+          client.update_environment(application_name: application_name,
+                                    environment_name: environment_name,
+                                    option_settings: [
+                                      {
+                                        namespace: "aws:autoscaling:asg",
+                                        option_name: "MinSize",
+                                        value: scale.begin.to_s
+                                      },
+                                      {
+                                        namespace: "aws:autoscaling:asg",
+                                        option_name: "MaxSize",
+                                        value: scale.end.to_s
+                                      }
+                                    ])
+
+          refresh
+        end
       end
     end
   end
